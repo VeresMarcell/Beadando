9efc3eea894b44e8c73ed7ae241d054f8b78c1f4
VeresMarcell <veresmarcell0727@gmail.com>
2020-04-23 17:31:37 +0200

    test

--- 10_feladat.py
+++ 10_feladat.py
@@ -1,12 +1,21 @@
-ins = int(input('Teszt esetek: '))
-if 1<= ins <=100:
+# ins = int(input('Teszt esetek: '))
+# if 1<= ins <=100:
+#     for i in range(ins):
+#         a, b = input('A és B: ').split()
+#         a = int(a)
+#         b = int(b)
+#         if 1 <= a <= 10**9 and 1 <= b <= 10**9:
+#             print((a+b)-10)
+#         else:
+#             print('A vagy B nem esik a megfelelő tartományba ([1,10^9])')
+# else:
+#     print('A teszt esetek száma nem megfelelő')
+
+ins = int(input('Number of instances: '))
+if 1 <= ins <= 100:
     for i in range(ins):
-        a, b = input('A és B: ').split()
-        a = int(a)
-        b = int(b)
-        if 1 <= a <= 10**9 and 1 <= b <= 10**9:
-            print((a+b)-10)
-        else:
-            print('A vagy B nem esik a megfelelő tartományba ([1,10^9])')
+        a = int(input('A: '))
+        b = int(input('B: '))
+        print((lambda a, b: ((a+b)-10) if 1<=b<=10**9 and 1<=b<=10**9 else print('A or B is not in the sufficient interval[1,10^9]'))(a,b))
 else:
-    print('A teszt esetek száma nem megfelelő')
+    print('Insufficient numver of instances')
