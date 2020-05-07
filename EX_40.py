64ae87dfca1158f63a0f0bc14aad493a424b0b64
VeresMarcell <veresmarcell0727@gmail.com>
2020-05-07 17:49:06 +0200

    Commit

--- EX_40.py
+++ EX_40.py
@@ -1,14 +1,26 @@
 import random
 
-n = random.randint(1,100)
-print(n)
-inp = int(input("Write your guess: "))
-while inp != n:
-    if inp > n:
-        print('Your guess is too big')
-        inp = int(input("Write your guess: "))
-    else:
-        print('Your guess is too small')
-        inp = int(input("Write your guess: "))
-
-print('You guessed right')
+while True:
+    menu = ['New Game (N)', 'Score (S)', 'Difficulty (D)', 'Quit (Q)']
+    for i in menu:
+        print(i)
+    click = input('Which option do you choose?>> ')
+    click = click.lower()
+    guess = 5
+    if click == 'n':
+        n = random.randint(1,100)
+        print(n)
+        inp = int(input("Write your guess>> "))
+        while guess > 1:
+            guess -= 1
+            if inp > n:
+                print('Your guess is too big')
+                inp = int(input("Write your guess>> "))
+            elif inp < n:
+                print('Your guess is too small')
+                inp = int(input("Write your guess>> "))
+            else:
+                print('You guessed right')
+                break
+    if click == 'q':
+        break
