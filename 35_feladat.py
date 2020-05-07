d9ce81d1eb1311b057010ceb74a744c7e8941fc4
VeresMarcell <veresmarcell0727@gmail.com>
2020-05-07 19:22:26 +0200

    zárójelvizsgálat

--- 35_feladat.py
+++ 35_feladat.py
@@ -9,23 +9,25 @@ def rgb():
     clr = clr.lower()
     if clr[:3] == 'rgb' or clr[:4] == 'rgba':       #betűk vizsgálata
         if clr[:4] =='rgba':
-            newCol = clr[5:len(clr)-1]      #számok leválasztása
-            nums = newCol.split(',')
-            if len(nums) == 4:      #elég szám van?
-                for i in range(len(nums)-1):        #számok tartománya
-                    if 0 > int(nums[i]) or 255 < int(nums[i]):
+            if clr[4] == '(' and clr['-1'] == ')':
+                newCol = clr[5:len(clr)-1]      #számok leválasztása
+                nums = newCol.split(',')
+                if len(nums) == 4:      #elég szám van?
+                    for i in range(len(nums)-1):        #számok tartománya
+                        if 0 > int(nums[i]) or 255 < int(nums[i]):
+                            return False
+                    if float(nums[3]) >= 1 or float(nums[3]) <= 0:
                         return False
-                if float(nums[3]) >= 1 or float(nums[3]) <= 0:
-                    return False
-                return True
+                    return True
         elif clr[:3] == 'rgb':      #betűk
-            newCol = clr[4:len(clr)-1]
-            nums = newCol.split(',')
-            if len(nums) == 3:  #szám
-                for i in nums:      #tartomány
-                    if 0 > int(i) or int(i) > 255:
-                        return False
-                return True
+            if clr[3] == '(' and clr[-1] == ')':
+                newCol = clr[4:len(clr)-1]
+                nums = newCol.split(',')
+                if len(nums) == 3:  #szám
+                    for i in nums:      #tartomány
+                        if 0 > int(i) or int(i) > 255:
+                            return False
+                    return True
     return False
 
 
